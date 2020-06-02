5d36011e2bc3bfb97d9dc6da1a5c4a0109023133
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 13:00:39 +0100

    Words can be entered into the form and appear on the page

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+
  
 const connection = "mongodb+srv://emily:mongo@cluster0-5zjjm.mongodb.net/test?retryWrites=true&w=majority"
 MongoClient.connect(connection, 
     { useUnifiedTopology: true })
   .then(client => {
+    console.log('Successfully connected to Database')
+    const db = client.db('translations')
+    const translations = db.collection('translations')
 
+
+    app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true }))
 
+
     app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html')   
+      db.collection('translations').find().toArray()
+        .then(results => {
+          res.render('index.ejs', { translations: results})
+        })
+        .catch()
+    
     })
     
     app.post('/words', (req, res) => {
@@ -26,10 +38,11 @@ MongoClient.connect(connection,
     app.listen(5000, function() {
         console.log('listening on 5000')
     })
-    console.log('Successfully connected to Database')
-    const db = client.db('translations')
-    const translations = db.collection('translations')
-  })
-  .catch(error => console.error(error))
+})
+.catch(console.error)
+  
+    
+  
+  
 
 
