c20a18d4b5f1d52fa650068622b8999fd5efb40b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 11:15:40 +0100

    Connection to the database has been established

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
  
+const connection = "mongodb+srv://emily:mongo@cluster0-5zjjm.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(connection, 
+    { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Successfully connected to Database')
+    const db = client.db('translations')
+  })
+  .catch(error => console.error(error))
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
