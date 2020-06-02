da37c63121cb5b13e0e2eae04f911195b3977c64
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 11:21:01 +0100

    Creates a collection, uses the insertOne method to add items to the collection

--- app.js
+++ app.js
@@ -7,22 +7,28 @@ const connection = "mongodb+srv://emily:mongo@cluster0-5zjjm.mongodb.net/test?re
 MongoClient.connect(connection, 
     { useUnifiedTopology: true })
   .then(client => {
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')   
+    })
+    
+    app.post('/words', (req, res) => {
+      translations.insertOne(req.body)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
+    
+    app.listen(5000, function() {
+        console.log('listening on 5000')
+    })
     console.log('Successfully connected to Database')
     const db = client.db('translations')
+    const translations = db.collection('translations')
   })
   .catch(error => console.error(error))
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')   
-})
-
-app.post('/words', (req, res) => {
-  console.log(req.body)
-})
-
-app.listen(5000, function() {
-    console.log('listening on 5000')
-})
